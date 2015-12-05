829ee55824799df88ca57216ff24a92aad3aaa9f
Soldatov, Nikita <nikita.soldatov.ext@wincor-nixdorf.com>
2015-12-05 22:14:39 +0300

    Restructure and additional clean up;

--- Build.cmd
+++ Build.cmd
@@ -1,6 +1,6 @@
 @echo off
 
 call "c:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat"
-msbuild /p:Configuration=Debug Slot_machine.sln
-msbuild /p:Configuration=Release Slot_machine.sln
+msbuild /p:Configuration=Debug slot_machine.sln
+msbuild /p:Configuration=Release slot_machine.sln
 pause>nul
